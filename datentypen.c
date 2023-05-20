49e7b990c1a4dc1c3cf34d742c631ac2304ee2ba
ketch <ketchu15@stud.thga.de>
2023-05-20 22:28:40 +0200

    Added C program demonstrating data types

---
+++ datentypen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int num = 10;
+    float decimal = 3.14;
+    char letter = 'A';
+    bool flag = true;
+
+    printf("Integer: %d\n", num);
+    printf("Float: %f\n", decimal);
+    printf("Character: %c\n", letter);
+    printf("Boolean: %s\n", flag ? "true" : "false");
+
+    return 0;
+}
+
+
